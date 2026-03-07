ee533e3fb2f6dc03e955a6e1584f0d1929237ecd
Thomas Neil <12561498+tn819@users.noreply.github.com>
2026-03-07 06:44:59 +0100

    Remove dist folder and add to gitignore

--- dist/dev/server/pages/_app.js
+++
@@ -1,5 +0,0 @@
-var R = require('../chunks/ssr/[turbopack]_runtime.js')('server/pages/_app.js')
-R.c('server/chunks/ssr/[root-of-the-server]__2e91cd7c._.js')
-R.c('server/chunks/ssr/[root-of-the-server]__bedbc402._.js')
-R.m('[project]/pages/_app.tsx [ssr] (ecmascript)')
-module.exports = R.m('[project]/pages/_app.tsx [ssr] (ecmascript)').exports
